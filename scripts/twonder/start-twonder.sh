de758df52d77c3e8aa330885638e89791f0c4b1e
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-03-07 20:40:54 +0100

    Put twonder script configs in /etc/wonder

--- scripts/twonder/start-twonder.sh
+++ scripts/twonder/start-twonder.sh
@@ -3,15 +3,14 @@
 NODE=$1
 
 # Read config
-SCRIPTDIR=`dirname $0`
-source ${SCRIPTDIR}/../configs/cluster.conf
-source ${SCRIPTDIR}/../configs/twonder_${NODE}.conf
+source /etc/wonder/cluster.conf
+source /etc/wonder/twonder_${NODE}.conf
 
 i=1;
 
 for NUM_CH in $CHANNELS;
 do
-    /opt/wonder/current/bin/twonder -c /opt/wonder/current/configs/twonder_config.xml -s /opt/wonder/current/configs/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
+    twonder -c /etc/wonder/twonder_config.xml -s /etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
     i=$(($i+1));
     sleep 1;
 done
