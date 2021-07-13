1a7c2fbdab42bf2df15141674fdf37c1d832272e
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-07-13 12:38:23 +0200

    [TWonder] Add additional args for setting verbosity

--- scripts/twonder/start-twonder.sh
+++ scripts/twonder/start-twonder.sh
@@ -8,7 +8,7 @@ i=1;
 
 for NUM_CH in $CHANNELS;
 do
-    twonder -c /etc/wonder/twonder_config.xml -s /etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
+    twonder ${TWONDER_ARGS} -c /etc/wonder/twonder_config.xml -s /etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
     i=$(($i+1));
     sleep 1;
 done
