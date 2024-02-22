5301a1bc3d663c1d7ba69aadadcd8889b145f699
Fares Schulz <schulzfares@gmail.com>
2024-02-22 14:12:38 +0100

    [meson build system] changed the config paths to /usr/local/etc

--- scripts/twonder/start-twonder.sh
+++ scripts/twonder/start-twonder.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Read config
-source /etc/wonder/cluster.conf
-source /etc/wonder/twonder.conf
+source /usr/local/etc/wonder/cluster.conf
+source /usr/local/etc/wonder/twonder.conf
 
 i=1;
 
 for NUM_CH in $CHANNELS;
 do
-    twonder ${TWONDER_ARGS} -c /etc/wonder/twonder_config.xml -s /etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
+    twonder ${TWONDER_ARGS} -c /usr/local/etc/wonder/twonder_config.xml -s /usr/local/etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
     i=$(($i+1));
     sleep 1;
 done
