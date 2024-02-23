1c5b0a29523c870a1bc50d52fdeb0c0686f004ed
Fares Schulz <schulzfares@gmail.com>
2024-02-23 19:35:27 +0100

    [scripts] labeled some of the outdated scripts

--- scripts/twonder/start-all-twonder.sh
+++ scripts/twonder/start-all-twonder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is outdated and is only necessary if we are not using systemd to manage the twonder instances.
+
 # Read config
 SCRIPTDIR=`dirname $0`
 source ${SCRIPTDIR}/../configs/cluster.conf
