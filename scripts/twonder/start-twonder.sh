0f25b8841bb410f6515785f80bab953c129a4d16
Fares Schulz <schulzfares@gmail.com>
2024-02-23 19:36:10 +0100

    [scripts] updated the twonder start script; no more unnecessary sleep

--- scripts/twonder/start-twonder.sh
+++ scripts/twonder/start-twonder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This is the script with which we start twonder instances, it is executed by the systemd service.
+
 # Read config
 source /usr/local/etc/wonder/cluster.conf
 source /usr/local/etc/wonder/twonder.conf
@@ -8,7 +10,7 @@ i=1;
 
 for NUM_CH in $CHANNELS;
 do
+    # for debugging the --verbose flag can be added to the command line
     twonder ${TWONDER_ARGS} -c /usr/local/etc/wonder/twonder_config.xml -s /usr/local/etc/wonder/speakers/${NODE}/twonder_speakerarray$i.xml -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
     i=$(($i+1));
-    sleep 1;
 done
