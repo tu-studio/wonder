fb66ae06dd0eb2e954dd83cef8ce712533be5f15
Fares Schulz <schulzfares@gmail.com>
2024-03-20 18:59:24 +0100

    [scripts] twonder args no more in use

--- scripts/start_twonder.sh
+++ scripts/start_twonder.sh
@@ -7,6 +7,6 @@ i=1;
 for file in /usr/local/etc/wonder/twonder/speaker-positions/twonder_*;
 do
     # for debugging the --verbose flag can be added to the command line
-    twonder ${TWONDER_ARGS} -c /usr/local/etc/wonder/twonder/twonder_config.xml -s $file -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
+    twonder -c /usr/local/etc/wonder/twonder/twonder_config.xml -s $file -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY &
     i=$(($i+1));
 done
