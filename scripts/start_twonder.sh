77fdf594d5a6f136e41f92ee1170cab123727a9f
Max Weidauer <max@weidauer.art>
2025-03-28 20:15:18 +0100

    updated start_twonder.sh to handle optional args better

--- scripts/start_twonder.sh
+++ scripts/start_twonder.sh
@@ -4,12 +4,34 @@
 
 i=1;
 
-# get cwonder port
-CWONDER_PORT="${CWONDER_PORT:-58100}"
 
-for file in /usr/local/etc/wonder/twonder/speaker-positions/twonder_*;
+for file in /home/leto/data/projects/arbeit/akt/seamless-configs/EN325/wonder/twonder/wintermute/speaker-positions/twonder_*;
+# for file in /usr/local/etc/wonder/twonder/speaker-positions/twonder_*;
 do
+    # setup args
+    args=(
+        -c /usr/local/etc/wonder/twonder/twonder_config.xml
+        -s $file
+        -i "$CWONDER_IP"
+        -j "twonder$i"
+        -o "$((58200 + $i))"
+        --negdelay "$NEG_DELAY"
+    )
+
+    # optional args based on set environment variables
+    if [[ -v MULTICAST_GROUP ]]; then
+        args+=(-g "$MULTICAST_GROUP")
+    fi
+
+    if [[ -v MULTICAST_PORT ]]; then
+        args+=(-t "$MULTICAST_PORT")
+    fi
+
+    if [[ -v CWONDER_PORT ]]; then
+        args+=(-p "$CWONDER_PORT")
+    fi
+    echo "${args[@]}"
     # for debugging the --verbose flag can be added to the command line
-    twonder -c /usr/local/etc/wonder/twonder/twonder_config.xml -s $file -i $CWONDER_IP -j twonder$i -o $((58200 + $i)) --negdelay $NEG_DELAY -p $CWONDER_PORT &
+    twonder  "${args[@]}" &
     i=$(($i+1));
 done
